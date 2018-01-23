17fa81bb8d055e1229601e46f978aa86b911ed40
蒋林均 <785137381@qq.com>
2018-01-23 17:15:50 +0800

    跑出来的工程保存

---
+++ SampleGenerator/SampleGenerator.h
@@ -0,0 +1,23 @@
+#include <cv.h>
+#include <cxcore.h>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\core\core.hpp>
+#include <opencv\cv.hpp>
+#include <time.h>
+#include <map>
+#include "../RandomForest/DepthUtilities.h"
+#include "../RandomForest/TemplateParser.h"
+using namespace std;
+void ReadOneFrame(std::string strDir, int nIndex, cv::Mat &mtxDepthPoints,
+	int** &pLabels, int &nWidth, int &nHeight, HandParams &param);
+void SelectSamples(const cv::Mat &mtxDepthPoints, cv::Rect rtOBB, int** pLabels,
+	int nNumPerClass, map<int, std::vector<cv::Point> > &mapSamples);
+void GenerateOneSample(const std::vector<DepthFeature> &vecFeatureIndices, cv::Point p, 
+	double fDepthValue, double f, double fu, double fv, int u0, int v0, 
+	const cv::Mat &mtxDepthPoints, double *pFeatureVec, int nLabel, FILE *pDestFile);
+void GenerateSamples(const std::vector<DepthFeature> &vecFeatureIndices,
+	double f, double fu, double fv, int u0, int v0, int nFeatureDim,
+	const cv::Mat &mtxDepthPoints, std::map<int, std::vector<cv::Point> > &mapSamples, FILE *pDestFile);
